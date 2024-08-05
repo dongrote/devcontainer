8b2dc2924699a5ecd1f60d37f210ab604a472c25
Don Grote <dongrote@recursiverhino.com>
2024-08-04 20:16:23 -0400

    add file tree and lualine

--- .config/nvim/lua/dongrote/remap.lua
+++ .config/nvim/lua/dongrote/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 -- open file explorer
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle, { desc = "File Tree Toggle", })
 
 -- set executable bit on current file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
@@ -16,4 +16,4 @@ vim.keymap.set("n", "<leader>npmt", "<cmd>!npm test<CR>")
 
 vim.keymap.set("n", "<leader>lsp", "<cmd>LspRestart<CR>")
 
-vim.keymap.set("n", "<leader>gd", "<cmd>lua require('omnisharp_extended').telescope_lsp_definition({ jump_type = 'vsplit' })<CR>")
+-- vim.keymap.set("n", "<leader>gd", "<cmd>lua require('omnisharp_extended').telescope_lsp_definition({ jump_type = 'vsplit' })<CR>")
