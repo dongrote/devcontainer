b14f664a82613c69ca4bed6fd397e211f187fd91
Don Grote <don.grote@gmail.com>
2024-08-08 19:30:00 -0400

    added LineNrAbove/LineNrBelow hi codes to set.lua

--- .config/nvim/lua/dongrote/set.lua
+++ .config/nvim/lua/dongrote/set.lua
@@ -2,6 +2,10 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
+-- Trying to change line numbers to be more visible
+-- vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'white', })
+-- vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'white', })
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
